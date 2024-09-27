57be007e138303c5cfaa84579b10e6a07bbf321b
Ashkan <ashkanghodrati01@gmail.com>
2024-09-27 06:48:13 -0700

    Add Account App

--- ShopProject/settings.py
+++ ShopProject/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "HomeApp.apps.HomeappConfig"
+    "HomeApp.apps.HomeappConfig",
+    "AccountApp.apps.AccountappConfig"
 ]
 
 MIDDLEWARE = [
@@ -106,6 +108,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
