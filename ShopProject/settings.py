483caf5bbf26b7e057df1911cfb349380ecd480f
Ashkan <ashkanghodrati01@gmail.com>
2024-09-27 08:10:18 -0700

    Add Profile App

--- ShopProject/settings.py
+++ ShopProject/settings.py
@@ -25,7 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "HomeApp.apps.HomeappConfig"
+    "HomeApp.apps.HomeappConfig",
+    "ProfileApp.apps.ProfileappConfig"
 ]
 
 MIDDLEWARE = [
